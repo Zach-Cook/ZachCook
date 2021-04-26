6bb9fbf4fd76fd3fea6c2665eea412eaf5c5c3d5
Zach_Cook_PC <zachcook8@gmail.com>
2021-04-26 07:24:25 -0500

    about text was updated for grammar

--- frontend/src/containers/about.js
+++ frontend/src/containers/about.js
@@ -18,7 +18,7 @@ export default function AboutContainer(){
                 <About.AboutFrameInner>
                     {/* <About.TitleText>About:</About.TitleText> */}
                     <About.TextFrame>
-                        <About.Text>- My name is Zach Cook. I graduated from the University of Kansas in 2019 with a Bachelors in Information Systems.</About.Text>
+                        <About.Text>- My name is Zach Cook. I graduated from the University of Kansas in 2019 with a Bachelor's in Information Systems.</About.Text>
                         <About.Text>- Although not jumping directly into the software industry after graduation, I worked for my family's organization to develop many useful applications.</About.Text>
                         <About.Text>- I also have a love for Crypto! From learning since 2016 to setting up ETH2.0 Testnet Validators, to creating <span style={aStyle} onClick={()=> openInNewTab("https://chainfeed.io")}>ChainFeed.io</span> with Web3.js and Chainlink oracles, I have an array of knowledge accross the industry.</About.Text>
                     </About.TextFrame>
